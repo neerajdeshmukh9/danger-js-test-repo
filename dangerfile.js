0db6501033bbf2dac8a1abfd094c9144051e5b6b
Neeraj Deshmukh <ts-neeraj.deshmukh@rakuten.com>
2021-10-28 15:15:51 +0530

    testing danger js

--- dangerfile.js
+++
@@ -1,8 +0,0 @@
-import {message, danger} from "danger"
-import { istanbulCoverage } from "danger-plugin-istanbul-coverage"
-
-const modifiedMD = danger.git.modified_files.join("- ")
-message("Changed Files in this PR: \n - " + modifiedMD)
-
-
-schedule(istanbulCoverage()) // Use default configuration
